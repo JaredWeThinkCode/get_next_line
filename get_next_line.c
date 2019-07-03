e0097b8dc55ed1f21f4c55f951efc96ed4e952b7
Jared NAIDOO <jnaidoo@c5r5s1.wethinkcode.co.za>
2019-07-03 17:07:24 +0200

    Final Push

--- get_next_line.c
+++ get_next_line.c
@@ -5,29 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jnaidoo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/08 18:14:06 by jnaidoo           #+#    #+#             */
-/*   Updated: 2019/06/14 08:21:01 by jnaidoo          ###   ########.fr       */
+/*   Created: 2019/06/27 16:06:10 by jnaidoo           #+#    #+#             */
+/*   Updated: 2019/07/03 16:52:50 by jnaidoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_next_line(int fd, char **a, char **line)
+static int		ft_next_line(int fd, char **a, char **line)
 {
-	char	*c;
-	int		d;
+	char	*b;
+	int		c;
 
-	d = 0;
-	while (a[fd][d] != '\n' && a[fd][d] != '\0')
-		d++;
-	if (a[fd][d] == '\n')
+	c = 0;
+	while (a[fd][c] != '\n' && a[fd][c] != '\0')
+		c++;
+	if (a[fd][c] == '\n')
 	{
-		*line = ft_strsub(a[fd], 0, d);
-		c = ft_strdup(a[fd] + d + 1);
+		*line = ft_strsub(a[fd], 0, c);
+		b = ft_strdup(a[fd] + c + 1);
 		free(a[fd]);
-		a[fd] = c;
+		a[fd] = b;
 	}
-	else if (a[fd][d] == '\0')
+	else if (a[fd][c] == '\0')
 	{
 		*line = ft_strdup(a[fd]);
 		ft_strdel(&a[fd]);
@@ -35,29 +35,29 @@ static int	ft_next_line(int fd, char **a, char **line)
 	return (1);
 }
 
-int			get_next_line(const int fd, char **line)
+int				get_next_line(int const fd, char **line)
 {
-	static char		*a[0];
-	char			buff[BUFF_SIZE + 1];
-	char			*b;
-	int				c;
+	static char	*a[1024];
+	char		buff[BUFF_SIZE + 1];
+	char		*b;
+	int			c;
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	if (a[fd] == NULL)
-		a[fd] = ft_strnew(1);
-	while ((c = read(fd, buff, BUFF_SIZE)) > 0)
+	while ((c = (read(fd, buff, BUFF_SIZE))) > 0)
 	{
 		buff[c] = '\0';
+		if (a[fd] == NULL)
+			a[fd] = ft_strnew(1);
 		b = ft_strjoin(a[fd], buff);
 		free(a[fd]);
 		a[fd] = b;
-		if (ft_strchr(buff, '\n'))
+		if (ft_strchr(a[fd], '\n'))
 			break ;
 	}
 	if (c < 0)
 		return (-1);
-	else if (c == 0 && a[fd][0] == '\0')
+	else if (c == 0 && (a[fd] == NULL || a[fd][0] == '\0'))
 		return (0);
 	return (ft_next_line(fd, a, line));
 }
