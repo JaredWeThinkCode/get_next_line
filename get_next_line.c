cc5480c0511e2e8372fa4356baafd96d6133ddfb
Jared NAIDOO <jnaidoo@c5r5s1.wethinkcode.co.za>
2019-07-05 13:19:36 +0200

    Final Push

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: jnaidoo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 16:06:10 by jnaidoo           #+#    #+#             */
-/*   Updated: 2019/07/03 16:52:50 by jnaidoo          ###   ########.fr       */
+/*   Updated: 2019/07/05 13:06:40 by jnaidoo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		ft_next_line(int fd, char **a, char **line)
+void	ft_store_line(int fd, char **a, char **line)
 {
 	char	*b;
 	int		c;
@@ -32,32 +32,41 @@ static int		ft_next_line(int fd, char **a, char **line)
 		*line = ft_strdup(a[fd]);
 		ft_strdel(&a[fd]);
 	}
-	return (1);
 }
 
-int				get_next_line(int const fd, char **line)
+int		ft_reader(char **a, int fd)
 {
-	static char	*a[1024];
-	char		buff[BUFF_SIZE + 1];
-	char		*b;
-	int			c;
+	char	buff[BUFF_SIZE + 1];
+	char	*b;
+	int		c;
 
-	if (fd < 0 || line == NULL)
-		return (-1);
 	while ((c = (read(fd, buff, BUFF_SIZE))) > 0)
 	{
-		buff[c] = '\0';
 		if (a[fd] == NULL)
 			a[fd] = ft_strnew(1);
+		buff[c] = '\0';
 		b = ft_strjoin(a[fd], buff);
 		free(a[fd]);
 		a[fd] = b;
 		if (ft_strchr(a[fd], '\n'))
 			break ;
 	}
+	return (c);
+}
+
+int		get_next_line(int const fd, char **line)
+{
+	static char	*a[1024];
+	int			c;
+
+	if (fd < 0 || line == NULL)
+		return (-1);
+	c = ft_reader(a, fd);
 	if (c < 0)
 		return (-1);
 	else if (c == 0 && (a[fd] == NULL || a[fd][0] == '\0'))
 		return (0);
-	return (ft_next_line(fd, a, line));
+	else
+		ft_store_line(fd, a, line);
+	return (1);
 }
